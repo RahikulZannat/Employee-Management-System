7444f8560df5824ac06483d8dfa65517b75a23cc
Rahikul <rahikuljannat@gmail.com>
2025-02-05 18:12:36 -0500

    Add users

--- Client/src/Components/Users.jsx
+++ Client/src/Components/Users.jsx
@@ -1,28 +1,41 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Users = () => {
-    const [users, setUsers] = useState ([{
-        Name: "Rahikul Zannat", Email: "rahik@gmail.com", Age: 27
-    }])
+  const [users, setUsers] = useState([
+    {Name: "Rahikul Zannat", Email: "rahik@gmail.com", Age: 27 },
+  ]);
+
   return (
-    <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
-        <div className='w-50 bg-white rounded p-3'>
-            <table className='table'>
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Age</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
+    <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
+      <div className="w-50 bg-white rounded p-3">
+        <Link to="/create" className="btn btn-success mb-3">Add User</Link>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Age</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+           {
+               users.map((user) =>{
+                return   <tr>
+                       <td>{user.Name}</td>
+                       <td>{user.Email}</td>
+                       <td>{user.Age}</td>
+                       <td><button>Edit</button><button>Delete</button></td>
+                   </tr>
 
-                    </tbody>
-            </table>
-        </div>
+               })
+           }
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Users
+export default Users;
